ce199a52519ec95a5665ce66a5d8a8d9bdf6f383
Quek Kai Yu <kaiyu92@hotmail.com>
2017-07-20 18:29:59 +0800

    Fixed bugs and added calendar

--- models/UserData.js
+++ models/UserData.js
@@ -5,7 +5,8 @@ var userDataSchema = new Schema({
   name: {type: String, required: true},
   org: String,
   content: String,
-  date: String
+  date: String,
+  user: String
 }, {collection: 'request'});
 
 module.exports = mongoose.model('UserData', userDataSchema);
